253ac1c5e4ccc73a5fd6f44521fe844e10513f35
Juan David Joya Ortiz <Ing.JuanJoya@outlook.com>
2014-07-01 20:57:04 -0500

    Beta 1
    SmartTech.Store  Version Beta 1

---
+++ JS/EjecutarLogin.js
@@ -0,0 +1,10 @@
+     function abrir(){ 
+		document.getElementById('fondot').style.display="block"; 
+		document.getElementById('contenido').style.display="block"; 
+		document.getElementById('cierro').style.display="block"; 
+	} 
+	function cerrar(){ 
+		document.getElementById('fondot').style.display="none"; 
+		document.getElementById('contenido').style.display="none"; 
+		document.getElementById('cierro').style.display="none"; 
+	} 

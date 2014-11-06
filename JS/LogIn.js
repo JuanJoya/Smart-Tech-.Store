5d95c7f9fe20fdbe322a95eb3f139dd246c9b848
Juan David Joya Ortiz <ing.juanjoya@outlook.com>
2014-11-05 21:39:32 -0500

    Beta 1.8
    Correciones en el Segundo <Section, cambios menores en el toggle del form>LogIn>

--- JS/LogIn.js
+++ JS/LogIn.js
@@ -5,15 +5,31 @@ var $form = $('#login'),
 	$button = $('#mostrar-form');
 
 // Eventos
+/*
 $button.click( mostrarFormulario );
 
 function mostrarFormulario(){
-	$form.slideToggle();
+	$form.slideToggle('fast','linear');
 	return false;
 }
+*/
 
-/*$("#caja1").hover(function () {
+$button.on('click', function () {
+	if($form.width()>500)
+	{
+		$form.toggleClass('login-expanded');
+	}
+	else
+	{
+		$form.toggleClass('login-expanded-2');
+	}
+    
+});
+/*
+$("#caja1").hover(function () {
     $(this).toggleClass("animated bounceInLeft");
-});*/
+});
+*/
 
-$("#caja1").addClass("animated bounceInLeft");
+$("#Title_1").addClass("animated bounceInLeft");
+$("#Title_2").addClass("animated bounceInLeft");

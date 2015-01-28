738fba1ccdbb44a38b782f3382d250a95cc00d12
Juan David Joya Ortiz <Ing.JuanJoya@outlook.com>
2015-01-28 17:45:59 -0500

    Beta 2.4
    Nuevo Doc para SignUp, Fixed Slider en Firefox, cambios ligeros en
    MobileDetail, se elimina el soporte para IE 9 e inferiores.

--- JS/Main.js
+++ JS/Main.js
@@ -52,3 +52,43 @@ $("#caja1").hover(function () {
     $(this).toggleClass("animated bounceInLeft");
 });
 */
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab').on('click', function (e) {
+  
+  e.preventDefault();
+  $(this).addClass('active');
+  $(this).siblings().removeClass('active');
+  target = $(this).children().attr('href');
+  $('.tab-content > div').not(target).hide();
+  $(target).fadeIn(600);
+  
+});
